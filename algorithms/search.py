bc386c24ef9cbc3602bc1b78611f10ba208d78c9
Elsa <e-lin@groovenauts.jp>
2016-12-05 18:05:28 +0900

    add search.py

--- algorithms/search.py
+++ algorithms/search.py
@@ -1,21 +1,49 @@
 # sequencial search : O(n)
-def sequencial_search(alist, item):
-    idx = 0
-    found = False
+def sequencial_search(lst, value):
+    for i in lst:
+        if i == value:
+            return True
+    return False
 
-    while idx < len(alist) and not found:
-        if alist[idx] == item:
-            found = True
-            break
-        idx += 1
-
-    return found
 
 # binary search : O(log(n))
+# @Assumes that data is sorted
+def binary_search(lst, value):
+    left = 0
+    right = len(lst)-1
+
+    while left <= right:
+        middle = left + (right-left)/2 # prevet overflow
+        print "middle = %s" % middle
+        if lst[middle] < value:
+            left = middle + 1
+        elif lst[middle] > value:
+            right = middle - 1
+        else:
+            return True
+    return False
+
+# binary search : O(log(n))
+# @Assumes that data is sorted
+def binary_search(lst, value, left, right):
+    if left > right:
+        return False
+    middle = left + (right-left)/2 # prevent overflow
+    if lst[middle] < value:
+        return binary_search(lst, value, middle+1, right)
+    elif lst[middle] > value:
+        return binary_search(lst, value, left, right-1)
+    else:
+        return True
 
 
 # hash search : O(1), load factor affects the bigO while collison happens
 
-testlist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
-print sequencial_search(testlist, 3)
-print sequencial_search(testlist, 13)
+lst = [1, 2, 32, 8, 17, 19, 42, 13, 0]
+# print sequencial_search(lst, 3)
+# print sequencial_search(lst, 13)
+lst.sort()
+# print binary_search(lst, 3)
+# print binary_search(lst, 13)
+print binary_search(lst, 3, 0, len(lst)-1)
+print binary_search(lst, 13, 0 , len(lst)-1)
